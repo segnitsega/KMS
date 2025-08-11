f0d8d1f5e9c2e411d961a1569f6a56b333719a60
bekam <bekamgenene@gmail.com>
2025-08-11 16:02:05 +0300

    update for start discustion at dashboard page

--- frontend/src/pages/documents.tsx
+++ frontend/src/pages/documents.tsx
@@ -1,7 +1,10 @@
 import Header from "@/components/reusable-header";
 import DocumentPageCard from "@/cards/documents/document-page-card";
+import UploadDocumentModal from "@/components/UploadDocumentModal";
+import { useState } from "react";
 
 const Documents = () => {
+  const [showUploadModal, setShowUploadModal] = useState(false);
   const documents = [
     {
       title: "Employee Onboarding Guide",
@@ -27,6 +30,12 @@ const Documents = () => {
   ];
   return (
     <div className="flex flex-col gap-6">
+      {showUploadModal && (
+        <UploadDocumentModal
+          onClose={() => setShowUploadModal(false)}
+          onUpload={() => setShowUploadModal(false)}
+        />
+      )}
       <Header
         title="Documents"
         subtitle="Manage and share organizational documents"
@@ -34,12 +43,13 @@ const Documents = () => {
         dropDownText="All Catagories"
         dropDownOptions={["All Catagories", "Engineering", "Marketing"]}
         searchPlaceholder="Search document..."
+        onButtonClick={() => setShowUploadModal(true)}
       />
 
       <div className="flex gap-6">
         {documents.map((doc, index) => (
           <DocumentPageCard
-          key={index}
+            key={index}
             title={doc.title}
             author={doc.author}
             date={doc.date}
