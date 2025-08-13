b89a62da80532fb36b1637e8857755e07ea09e00
segnitsega <segnitsega6@gmail.com>
2025-08-13 21:05:12 +0300

    configure cors correctly

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,15 +13,12 @@ dotenv.config();
 const port = process.env.port;
 const server = express();
 
-const allowedOrigins = ["http://localhost:5173"];
 server.use(
   cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin as string)) {
-        callback(null, true);
-      }
-    },
+    origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
