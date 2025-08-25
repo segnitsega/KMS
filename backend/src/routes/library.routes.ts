fcefd23ffd710ad30bb557daa6f26afe3e3f875b
segnitsega <segnitsega6@gmail.com>
2025-08-25 15:08:02 +0300

    add removeBookFromUserLibrary to library routes

--- backend/src/routes/library.routes.ts
+++ backend/src/routes/library.routes.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { addBook, addToUserLibrary, getBookById, getBooks, getUserLibrary } from "../controllers/library.controller"
+import { addBook, addToUserLibrary, getBookById, getBooks, getUserLibrary, removeBookFromUserLibrary } from "../controllers/library.controller"
 import { memoryUpload } from "../middlewares/upload.middleware"
 import { verifyToken } from "../middlewares/auth.middleware"
 
@@ -8,6 +8,7 @@ export const  libraryRouter = express.Router()
 libraryRouter.use(verifyToken)
 libraryRouter.get("/", getBooks)
 libraryRouter.get("/user/:id", getUserLibrary)
+libraryRouter.delete("/user/:id", removeBookFromUserLibrary)
 libraryRouter.post("/user/:id", addToUserLibrary)
 libraryRouter.post("/upload-book", memoryUpload.single("book"), addBook)
 libraryRouter.get("/:id", getBookById)
