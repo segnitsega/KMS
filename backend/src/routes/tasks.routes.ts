586d9878ef6e8a11d14e94e03ca1eaeb3b251c7f
segnitsega <segnitsega6@gmail.com>
2025-09-01 10:54:55 +0300

    implements task submission route

--- backend/src/routes/tasks.routes.ts
+++ backend/src/routes/tasks.routes.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import { verifyToken } from "../middlewares/auth.middleware"
-import { getTaskById, getTasks, getUserTasks } from "../controllers/task.controller"
+import { getTaskById, getTasks, getUserTasks, handleSubmitTask } from "../controllers/task.controller"
+import { memoryUpload } from "../middlewares/upload.middleware"
 
 export const taskRouter = express.Router()
 
 taskRouter.use(verifyToken)
 taskRouter.get("/", getTasks)
 taskRouter.get("/user", getUserTasks)
+taskRouter.post("/submit", memoryUpload.single("task") ,handleSubmitTask)
 taskRouter.get("/:id", getTaskById)
