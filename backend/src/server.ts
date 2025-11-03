d45cd8121e6c62712bbc918431bdc4f21d116be3
segnitsega <segnitsega6@gmail.com>
2025-11-03 12:20:35 +0300

    make cors to accept every origin for deployment testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,8 @@ const server = express();
 
 server.use(
   cors({
-    origin: ["http://localhost:5173", "http://192.168.45.162:5173"],
+    // origin: ["http://localhost:5173", "http://192.168.45.162:5173"],
+    origin: '*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
